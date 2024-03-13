95ba7d984f4c53872fb79951c72613627ec39ea7
Kehinde ojo <ojok761@gmail.com>
2024-03-13 18:19:39 -0400

    feature: Added annaylatics to the site

--- photo/src/app/layout.js
+++ photo/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 import Nav from "@/Component/Nav";
 
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
         <Nav/>
         {children}
         <SpeedInsights/>
+        <Analytics/>
         </body>
     </html>
   );
